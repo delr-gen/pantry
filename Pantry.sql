43c0f7db2744dfd1ea0031faaf59b51ec1366ea8
Genevieve del Rosario <genevievedelrosario@Genevieves-MacBook-Pro.local>
2026-04-20 22:34:30 -0700

    Fixed Pantry DB and added more recipes and ingredients

--- Pantry.sql
+++ Pantry.sql
@@ -8,31 +8,34 @@ CREATE TABLE IF NOT EXISTS Ingredients (
   
 CREATE TABLE IF NOT EXISTS Pantry_Ingredients (
   pantry_ingredient_id INT PRIMARY KEY,
-  date_bought DATE,
+  date_bought DATE DEFAULT CUR_DATE(),
   expiration_date DATE,
-  quantity FLOAT NOT NULL,
-  unit INT NOT NULL,
+  quantity FLOAT,
+  unit VARCHAR(20) DEFAULT "unit",
   FOREIGN KEY (pantry_ingredient_id) REFERENCES Ingredient(ingredient_id)
 );
 
 CREATE TABLE IF NOT EXISTS Recipe (
   recipe_id INT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  serving_size INT NOT NULL
+  serving_size INT,
+  mins INT
 ); 
 
 CREATE TABLE IF NOT EXISTS Steps_in_Recipe (
-  step_id INT PRIMARY KEY,
-  recipe_id INT NOT NULL,
+  step_id INT,
+  recipe_id INT,
   instruction VARCHAR(300) NOT NULL,
+  PRIMARY KEY (step_id, recipe_id),
   FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id)
 );
   
 CREATE TABLE IF NOT EXISTS Ingredients_in_Recipe (
-  ingredient_id INT NOT NULL,
-  recipe_id INT NOT NULL,
+  ingredient_id INT,
+  recipe_id INT,
   quantity FLOAT,
   unit VARCHAR(15),
+  PRIMARY KEY (ingredient_id, recipe_id),
   FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id),
   FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id)
 );
