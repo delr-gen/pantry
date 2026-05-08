42225ab3c215afa920a57b221499d5bda2a5681e
Genevieve del Rosario <genevievedelrosario@Genevieves-MacBook-Pro.local>
2026-05-07 18:43:04 -0700

    changed build process

--- app/src/main/webapp/src/App.tsx
+++ app/src/main/webapp/src/App.tsx
@@ -1,9 +1,17 @@
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 import SearchBar from "./components/searchbar.tsx"
 
 function App() {
 
   return (
     <>
+      <BrowserRouter basename={process.env.REACT_APP_ROUTER_BASE || ''}></BrowserRouter>
       <SearchBar/>
     </>
   )
